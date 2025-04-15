40239192a5d4a02c648accb2731e928d5ac8c201
liz-rios-14 <rioslizbeth37@gmail.com>
2025-04-15 10:59:33 -0500

    Agregar archivos de proyecto.

---
+++ Calculadora_TI/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Calculadora_TI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.btnRestar = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblNum1.Location = new System.Drawing.Point(26, 31);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(68, 16);
+            this.lblNum1.TabIndex = 0;
+            this.lblNum1.Text = "Numero 1:";
+            this.lblNum1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblNum2.Location = new System.Drawing.Point(25, 82);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(68, 16);
+            this.lblNum2.TabIndex = 1;
+            this.lblNum2.Text = "Numero 2:";
+            this.lblNum2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblRes
+            // 
+            this.lblRes.AutoSize = true;
+            this.lblRes.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblRes.Location = new System.Drawing.Point(25, 202);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(69, 16);
+            this.lblRes.TabIndex = 2;
+            this.lblRes.Text = "Resultado";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(166, 31);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 3;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(166, 75);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 4;
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(166, 195);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(100, 22);
+            this.txtRes.TabIndex = 5;
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Image = ((System.Drawing.Image)(resources.GetObject("btnSumar.Image")));
+            this.btnSumar.Location = new System.Drawing.Point(38, 119);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(55, 27);
+            this.btnSumar.TabIndex = 6;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // btnRestar
+            // 
+            this.btnRestar.Location = new System.Drawing.Point(113, 119);
+            this.btnRestar.Name = "btnRestar";
+            this.btnRestar.Size = new System.Drawing.Size(60, 27);
+            this.btnRestar.TabIndex = 7;
+            this.btnRestar.Text = "Restar";
+            this.btnRestar.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(189, 119);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(77, 27);
+            this.btnMultiplicar.TabIndex = 8;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 295);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnRestar);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtRes;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Button btnRestar;
+        private System.Windows.Forms.Button btnMultiplicar;
+    }
+}
+
---
+++ Calculadora_TI/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_TI
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+                double num1 = Convert.ToDouble(txtNum1.Text);
+                double num2 = Convert.ToDouble(txtNum2.Text);
+                double suma = num1 + num2;
+                txtRes.Text = suma.ToString();
+            }
+
+            catch (FormatException)
+
+            {
+                MessageBox.Show("Por favor, ingrese valores numéricos válidos.");
+            }
+        }
+    }
+}
